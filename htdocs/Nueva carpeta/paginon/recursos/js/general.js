cccf9f04a6c242672a32b9c9039cf317a592145e
unodedaw <unodedaw@gmail.com>
2013-01-10 16:28:03 +0100

    ff

--- htdocs/Nueva carpeta/paginon/recursos/js/general.js
+++
@@ -1,6 +0,0 @@
-	function muestrahijos(id){
-			document.getElementById(id).style.display="block";
-		}
-		function ocultahijos(id){
-			document.getElementById(id).style.display="none";
-		}
--- htdocs/asignaturas/Sistemas____/MegaCalcu/HxDec.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$deci=$_POST['numdec'];
-	$number = hexdec ( "$deci" );	
-	echo $_POST['numdec'].'<sub>16</sub> <----> '.$number.'<sub>10</sub>';		
-?>
